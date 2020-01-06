60023211afa986343dd4716a9104e7d29564fc81
qseboy <qseboy1998@gmail.com>
2020-01-06 10:51:41 +0200

    #11 add store refactoring

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { addPost } from "./redux/state";
-import { newPostMessage } from "./redux/state";
 
 //Прорисовка UI Tree(Дерева)
-let rerenderApp = () => {
+let rerenderApp = state => {
   ReactDOM.render(
-    <App state={state} addPost={addPost} newPostMessage={newPostMessage} />,
+    <App
+      state={state}
+      addPost={store.addPost.bind(store)}
+      newPostMessage={store.newPostMessage.bind(store)}
+    />,
     document.getElementById("root")
   );
 };
 
-//Вызов прорисовки, в файле render лежит функция.
-rerenderApp(state);
-
-subscribe(rerenderApp); //Вызываю функцию, callback fun subscribe которая вызывает rerenderApp
+rerenderApp(store.getState()); //Вызов прорисовки, в файле render лежит функция.
+store.subscribe(rerenderApp); //Вызываю функцию, callback fun subscribe которая вызывает rerenderApp
 
 serviceWorker.unregister();
